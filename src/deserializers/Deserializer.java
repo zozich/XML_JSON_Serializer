31722f1cd68cf3801c474c738fc76a38d22d6b55
zozich <zozich2010@gmail.com>
2015-06-11 16:33:19 +0300

    deserializer deleted

--- src/deserializers/Deserializer.java
+++
@@ -1,9 +0,0 @@
-package deserializers;
-
-import serializers.SerializerType;
-import serializers.StringSerializable;
-
-public interface Deserializer {
-
-  StringSerializable deserialize(String serialized, SerializerType type);
-}
